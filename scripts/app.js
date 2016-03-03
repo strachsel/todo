069265c1e7c713259a1fa85968e7382c92412b5a
Sonya Trachsel <sofyatrachsel@gmail.com>
2016-03-03 15:15:06 -0800

    Added a controller to click event for the save button

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,6 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+.controller('mainCtrl', function($scope){
+  $scope.helloWorld = function() {
+    console.log("Hello there! this is the helloWorld controller function, in the mainCtrl");
+  };
+});
--- scripts/hello-world.js
+++
@@ -1,6 +0,0 @@
-angular.module('todoListApp')
-.directive('helloWorld', function(){
-  return {
-    template: 'This is the hello world directive!',
-  };
-});
